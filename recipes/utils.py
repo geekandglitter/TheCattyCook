5673385a99f8e2ca5452f9f89e04197a4a2839bd
Linda <stayathomegeek@gmail.com>
2021-01-22 19:06:11 -0500

    Added some comments for clarity

--- recipes/utils.py
+++ recipes/utils.py
@@ -8,83 +8,46 @@ def search_func(user_terms):
     # 1) to get an "and" condition instead of "or", just add one filter after another.
     # However, what I really want is an "or" condition which I will later rank
     # in order of number of search hits for each recipe.
-    # 2) values() would have produced a dictionary.
+    # 2) values() would have produced a dictionary. Instead I used values_list
     # 3) I could have used an "or" pipe character with just one filter, but then I can't easily retrieve which search hits
     # were found. So instead I am doing multiple queries, one for each search term.
 
-    # Find out how many search terms the user inputted, and get two lists ready for them
+    # Find out how many search terms the user inputted, and get three lists ready for them, 
+    # as we have 3 ways we need to massage the data
     num_terms = len(user_terms)    
     queryset=[None] * num_terms # rather than start with an empty list, initialize it with None
     q_converted=[None] * num_terms  # this will be for when we convert from list of tuples to list of lists
-    listset = [None] * num_terms
-    # Now I want to add function to allow excludes   
-    # First, I want to see what the user typed. If the search term starts with a minus,
-    # then I have to write exclude code 
-    # Maybe like this: MyModel.objects.exclude(id__in=[id1,id2,id3 ])
-
-
-
-
-
-
-  
+    listset = [None] * num_terms 
 
     # Now set up the query. A side effect of using icontains is that if the user searches for a word such as "bed," 
-    # there will be search hits, such as "cubed." But I don't care. 
-    """
-    # FROM STACKOVERFLOW:
-    # I found a solution to the problem by doing it in two steps:
-
-    # Get the queryset with all advisories
-    queryset = Advisory.objects.all()
-    # Find the products the customer doesn't care about
-    blacklist = Customer.objects.get(pk=customer_id).blacklist.all()
-    # Build a whitelist of products excluding the blacklist from the customer
-    whitelist = Product.objects.all().exclude(id__in=blacklist)
-    # And filter the queryset with the new whitelist
-    queryset = queryset.filter(product_names__in=whitelist).distinct()
-    ###########################
-    ## THIS IS MY OLD CODE #### 
-    exclude_term = "caper"    
-    for i, term in enumerate(user_terms):
-        q[i] = list(AllContents.objects.filter(fullpost__icontains=term)
-                                        .exclude(fullpost__icontains=exclude_term) 
-                                        .values_list('hyperlink', 'title')  
-                    ) 
-    ###########################
-    """
+    # there will be search hits, such as "cubed." But I don't care.  
+    
+    # See if the user has requested one or more ingredients to be excluded. They would do this with a minus sign.
     unwanted_ingredients = []
     for term in user_terms:
         if term[0]=="-":
             unwanted_ingredients.append(term[1:]) 
-  
+            # RIGHT HERE REMOVE THE UNWANTED TERMS FROM USER_TERMS 
+    # Note: Looking at the above code, I think I'm being slightly wasteful because I'm including the unwanted, and then
+    # below, I am taking the unwanted back out. It would be better to remove the unwanted terms from user_terms
      
-    i=0
+    # Now get all the recipes that match all the user search terms
     for i, term in enumerate(user_terms):
         queryset[i] = AllContents.objects.filter(fullpost__icontains=term)\
-                                         .values_list('hyperlink', 'title')   # We now have a list of querysets
-    
-     
+                                         .values_list('hyperlink', 'title')   # We now have a list of querysets  
 
-    # now for each of the querysets above, I want to filter out the negative (assuming for now there's only one)
-     
-
-    # Right here is where I look for unwanted ingredients. But there might not be any.
-    #unwanted_ingredients = ["capers", "wine", "ginger"]
+    # Loop through any unwanted ingredients and exclude them    
     for neg_term in unwanted_ingredients:
         for j in range(0,num_terms):
-            queryset[j] = queryset[j].exclude(fullpost__icontains=neg_term)  
-             
+            queryset[j] = queryset[j].exclude(fullpost__icontains=neg_term)               
     listset = list(queryset)   
 
-    # So now we have querysets which each contains a list of tuples. We need to convert to a list of lists.
-     
+    # So now we have one or more querysets (one queryset for each search term)
+    # each of which each contains a list of tuples. We need to convert the list(s) of tuples to list(s) of lists.     
     for j in range(0, num_terms): # convert to a list of lists
-
         q_converted[j]=list(map(list, listset[j]))     
- 
 
-    # Now stick the search term(s) we found intoeach query result so that we can later show the user all the terms satisfied by each recipe
+    # Now stick the search term(s) we found into each query result so that we can later show the user all the terms satisfied by each recipe
     for i, term in enumerate(user_terms): # this shows the search terms in the user's order
         for recipe in q_converted[i]:
             recipe.insert(0, term)     
@@ -99,7 +62,7 @@ def search_func(user_terms):
     for term in user_terms:
         user_search_terms = user_search_terms + "<br>" + term
 
-    # If the combined list is empty, then return now    
+    # If the combined list is empty, then we can go ahead and return now, and tell user there are no results   
     if not combined_list:
         count = 0         
         trimmed_list = [['None']]
