6897e0b146541b27bf0d48140b02aa1af55ebdb9
Linda <stayathomegeek@gmail.com>
2021-02-15 16:40:28 -0500

    Added a line for ALLOWED_HOSTS to make it handy for repl.it

--- recipes/settings.py
+++ recipes/settings.py
@@ -49,10 +49,9 @@ APPSEARCH_INDEXING_ENABLED = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['stayathomegeek.pythonanywhere.com', '127.0.0.1', 'thecattycook.geekandglitter.repl.co']
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -67,13 +66,7 @@ INSTALLED_APPS = [
   
 
 ]
-'''
-Can be erased:
-ALLOWED_HOSTS = ['stayathomegeek.pythonanywhere.com', '127.0.0.1', 
-                 '* @geekandglitter', '.localhost', '[::1]', '[*]',
-                 '1944af39-b599-40cf-a6ba-c1a9f3897dcc.id.replitusercontent.com', 
-                 'thecattycook.geekandglitter.repl.co']
-'''
+ 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
